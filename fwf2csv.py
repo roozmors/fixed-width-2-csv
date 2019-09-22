71e3284b2c7cee766d799e9b29af989f89d8720f
Roozbeh Morsali <55666795+roozmors@users.noreply.github.com>
2019-09-23 04:24:50 +1000

    Add files via upload

---
+++ fwf2csv.py
@@ -0,0 +1,126 @@
+
+class FWF2CSV:
+    """ The methods in this class create and write a fixed-width-file and read/write it into a csv file."""
+    def __init__(self,dataset,config_dict):
+        self.dataset = dataset
+        self.config_dict = config_dict
+    
+
+    
+    def fix_dict(self,data_dict,config,max_length_data):
+        """ This function fills the empty cells (end of the list) with default values from config file"""
+        for field_name in data_dict:
+            if len(data_dict[field_name])<max_length_data:
+                extesion_list = [config[field_name]['default_value'] for i in range(max_length_data-len(data_dict[field_name])) ]
+                data_dict[field_name].extend(extesion_list)
+                
+        return data_dict   
+
+    def fix_data_type(self,data_dict, config):
+        data_dict = self.fix_cols_type(data_dict, config)
+        data_dict = self.fix_cols_length(data_dict, config)
+        return data_dict
+
+    def fix_cols_type(self,data_dict, config):
+        """If col type is string, change the data to string. If int or float, try to convert"""
+        for col_name in config:
+            if not isinstance(data_dict[col_name],config[col_name]['type']):
+                if config[col_name]['type'] is str:
+                    data_dict[col_name] = str(data_dict[col_name])
+                elif config[col_name]['type'] is int:
+                    try:
+                        data_dict[col_name] = int(data_dict[col_name])
+                    except ValueError:
+                        print('{} cant be converted into int format'.format(data_dict[col_name]))
+                elif config[col_name]['type'] is float:
+                    try:
+                        data_dict[col_name] = float(data_dict[col_name])
+                    except ValueError:
+                        print('{} cant be converted into float format'.format(data_dict[col_name]))
+                
+        return data_dict
+
+    def fix_cols_length(self,data_dict,config):
+        """if length of data in a string-type column is greater than it should be (i.e. =length), pick first 'length' values. 
+        If type is boolean, maybe the input is True, or 1 or yes (etc.), then change the output to 1 
+        data_dict, contains one series of data. """ 
+        for col_name in config:
+            if config[col_name]['type'] is str:
+                if len(str(data_dict[col_name])) > config[col_name]['length']:
+                    data_dict[col_name] = data_dict[col_name][:config[col_name]['length'] ] # first length characters
+            elif config[col_name]['type'] is bool:
+                if data_dict[col_name] in {1,'yes','Yes','YES','True'}:
+                    data_dict[col_name]=1
+                elif data_dict[col_name] in {0,'no','No','NO','False'}:
+                    data_dict[col_name]=0
+                else:
+                    raise ValueError("{} is not a bolean type.".format(data_dict[col_name]) )
+            
+            else:
+                if len(str(data_dict[col_name])) > config[col_name]['length']:
+                    raise ValueError("{} cannot fit into {} spaces).".format(data_dict[col_name], config[col_name]['length']))
+
+        return data_dict
+
+    def make_new_line(self,data_dict,config,encoding = 'utf-8'):
+        """ Makes new line with provided input (data) using configuration file (config)"""
+        line = "".join(['{0:{1}}'.format(data_dict[col_name],config[col_name]['length']) 
+                        for col_name in config])+'\n'
+        
+        line = line.encode(encoding = encoding, errors ='replace')
+        return line
+
+    def break_dict(self,data_dict,config):
+        # when there are more than one entry for each column in the original dictionary
+        # some of fields might be empty, find the maximum length
+        max_length_data = 0
+        for field_name in data_dict:
+            max_length_data = max(len(data_dict[field_name]),max_length_data)
+        
+        # fill the empty cells with default value from config 
+        data_dict = self.fix_dict(data_dict,config,max_length_data)
+        
+        nested_dict = dict.fromkeys([f_name for f_name in config])
+        new_dict = {str(i):dict(nested_dict) for i in range(max_length_data)}
+        
+        for field_name in config:
+            for idx,_ in  enumerate(data_dict[field_name]):
+                new_dict[str(idx)][field_name]= data_dict[field_name][idx]
+
+        for col_name in new_dict:
+            new_dict[col_name] = self.fix_data_type(new_dict[col_name],config)
+            
+        return new_dict
+
+
+    def write_file(self,data_dict,config, file_name = 'new_file.txt', encoding = 'cp1252'):
+    
+        segregated_dict = self.break_dict(data_dict,config)
+        header_line = "".join(['{0:{1}}'.format(col_name,int(config[col_name]['length'])) 
+                            for col_name in config])+'\n'
+        header_line=header_line.encode(encoding)
+        with open(file_name,"wb") as f:
+            f.write(header_line)
+            for dct in segregated_dict:
+                f.write(self.make_new_line(segregated_dict[dct],config,encoding = encoding))    
+
+    def chopper(self,str_obj, config):
+        position = 0
+        chopped_string = []
+        for field_config in config.values():
+            length = field_config['length']
+            chopped_string.append(str_obj[position:position + length])
+            position += length
+        return chopped_string
+
+    def joiner(self,chopped_line,delimiter=','):
+        return delimiter.join(chopped_line)+'\n'
+
+    def fwf_2_csv_line(self,line, config, delimiter = ','):
+        chopped_line = self.chopper(line,config)
+        joined_line = self.joiner(chopped_line,delimiter)
+        return joined_line
+
+
+    
+
---
+++ test_fwf2csv.py
@@ -0,0 +1,253 @@
+import unittest
+import json
+import os 
+
+from fwf2csv import FWF2CSV
+
+class TestFWF2CSV(unittest.TestCase):
+    # tc stands for test_case
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    
+    def setUp(self):
+        self.base_test_case = {
+            'f1': ['ab','AB','AAABBB_B'],
+            'f2': ['cd','CD','CCC_DDD_D'],
+            'f3': ['ef','EF','EFFF_FFF'],
+            'f4': ['gh','GH','GHGHGHGH'],
+            'f5': ['ij','IJ','IJIJIJIJ'],
+            'f6': ['kl','KL','KL___KL'],
+            'f7': ['mn','MN','MN123456MN'],
+            'f8': ['op','OP','OPOP'],
+            'f9': ['qr','QR','QRqrQRqr'],
+            'f10': ['st','ST','STSTstST']
+        }
+
+        with open ('spec.json','r') as jf:
+            specs_dict = json.load(jf)
+        self.base_config_dict = {col_name:{'length': int(length_val), 'type': str,'default_value' : ''} 
+               for (col_name,length_val) in zip(specs_dict['ColumnNames'], specs_dict['Offsets'])}
+
+        self.base_tc_isntance = FWF2CSV(self.base_test_case,self.base_config_dict)
+
+            
+    def tearDown(self):
+        print('tearDown\n')
+    
+    def test_fix_dict(self):
+        tc_1 = {
+            'f1': ['ab','AB',12345678],
+            'f2': [1],
+            'f3': ['ef','EF'],
+        }
+
+        config_1 = {
+            'f1': {'length': 5, 'type': str, 'default_value' : ''},
+            'f2': {'length': 12, 'type': int, 'default_value' : -1},
+            'f3': {'length': 3, 'type': str, 'default_value' : ''}
+        }
+
+        EA_1 = {
+            'f1': ['ab','AB',12345678],
+            'f2': [1,-1,-1],
+            'f3': ['ef','EF',''],
+        }
+        
+        self.assertEqual(FWF2CSV.fix_dict(self,tc_1,config_1,3),EA_1)
+        
+    def test_fix_data_type(self):
+        # type f1 from int to str, type f2 from str to int, type f3 from bool to str, 
+        # length f1 should be cut to 5
+        tc_2 = {
+            'f1': 12345678,
+            'f2': '1',
+            'f3': True,
+        }
+
+        config_2 = {
+            'f1': {'length': 5, 'type': str, 'default_value' : ''},
+            'f2': {'length': 12, 'type': int, 'default_value' : -1},
+            'f3': {'length': 6, 'type': str, 'default_value' : ''}
+        }
+
+        EA_2 = {
+            'f1': '12345',
+            'f2': 1,
+            'f3': 'True',
+        } 
+
+        self.assertEqual(FWF2CSV.fix_data_type(FWF2CSV(tc_2,config_2),tc_2,config_2),EA_2)
+
+    def test_fix_cols_type(self):
+        # f1 is a string and can't be converted to int
+        tc_3 = {
+            'f1': 'ab',
+            'f2': '1',
+        }
+
+        config_3_1 = {
+            'f1': {'length': 5, 'type': int, 'default_value' : ''},
+            'f2': {'length': 12, 'type': int, 'default_value' : -1},
+        }
+
+        self.assertRaises( (TypeError,ValueError),FWF2CSV.fix_cols_type(self,tc_3,config_3_1))
+
+        config_3_2 = {
+                    'f1': {'length': 5, 'type': float, 'default_value' : ''},
+                    'f2': {'length': 12, 'type': int, 'default_value' : -1},
+                }
+        self.assertRaises( (TypeError,ValueError),FWF2CSV.fix_cols_type(self,tc_3,config_3_2))
+
+    def test_fix_cols_length(self):
+        # f1 should be cut to 5 letters. f2 with config_4_1 should be fine
+        # but when change the length to 2, 123 can't fit into 2 spaces field, should raise error
+        # same with config_4_3 in which f2 column type is float
+        tc_4 = {
+            'f1': 'abcdef',
+            'f2': 123,
+        }
+
+        config_4_1 = {
+            'f1': {'length': 5, 'type': str, 'default_value' : ''},
+            'f2': {'length': 3, 'type': int, 'default_value' : -1},
+        }
+
+        EA_4 = {
+            'f1': 'abcde',
+            'f2': 123,
+        }
+
+        self.assertEqual( FWF2CSV.fix_cols_length(self,tc_4,config_4_1),EA_4)
+
+        config_4_2 = {
+                    'f1': {'length': 5, 'type': str, 'default_value' : ''},
+                    'f2': {'length': 2, 'type': int, 'default_value' : -1},
+                }
+        self.assertRaises( (TypeError,ValueError),FWF2CSV.fix_cols_type(self,tc_4,config_4_2))
+
+        config_4_3 = {
+                    'f1': {'length': 5, 'type': str, 'default_value' : ''},
+                    'f2': {'length': 2, 'type': float, 'default_value' : -1},
+                }
+        self.assertRaises( (TypeError,ValueError),FWF2CSV.fix_cols_type(self,tc_4,config_4_3))
+
+
+    def test_make_new_line(self):
+        # should return line in binary format
+        tc_5 = {
+            'f1': 'abcde',
+            'f2': 123,
+        }
+        
+        config_5= {
+            'f1': {'length': 7, 'type': str, 'default_value' : ''},
+            'f2': {'length': 3, 'type': int, 'default_value' : -1}
+            }
+
+        EA_5 = b'abcde  123\n'
+        self.assertEqual(FWF2CSV.make_new_line(self,tc_5,config_5),EA_5)
+
+    
+
+    def test_break_dict(self):
+        tc_6 = {
+            'f1': ['ab','AB','12345'],
+            'f2': [1,0,2],
+            'f3': ['ef','EF','EFG'],
+        }
+
+        config_6 = {
+            'f1': {'length': 5, 'type': str, 'default_value' : ''},
+            'f2': {'length': 12, 'type': int, 'default_value' : -1},
+            'f3': {'length': 3, 'type': str, 'default_value' : ''}
+        }
+
+        EA_6 = {'0':{'f1': 'ab', 'f2': 1, 'f3': 'ef'},
+                '1':{'f1': 'AB', 'f2': 0, 'f3': 'EF'},
+                '2':{'f1': '12345', 'f2': 2, 'f3': 'EFG'}
+        }
+
+        self.assertEqual(FWF2CSV.break_dict(FWF2CSV(tc_6, config_6), tc_6, config_6), EA_6)
+
+    def test_write_file(self):
+        # should write file to local directory 
+        file_name = 'test_new_file.txt'
+        FWF2CSV.write_file(self.base_tc_isntance,
+        self.base_test_case, self.base_config_dict, file_name=file_name)
+        self.assertTrue(os.path.exists(file_name))
+
+    def test_chopper(self):
+        tc_7 = '  fo   12   bar'
+
+        config_7 = {
+            'f1': {'length': 4, 'type': str, 'default_value' : ''},
+            'f2': {'length': 5, 'type': int, 'default_value' : -1},
+            'f3': {'length': 6, 'type': str, 'default_value' : ''}
+        }
+        EA_7 =['  fo','   12','   bar']
+        self.assertEqual(FWF2CSV.chopper(self,tc_7,config_7),EA_7)
+
+    def test_joiner(self):
+        tc_8 = ['  fo','   12','   bar']
+
+        EA_8 ='  fo,   12,   bar\n'
+        self.assertEqual(FWF2CSV.joiner(self,tc_8),EA_8)  
+
+    def test_fwf_2_csv_line(self):
+        tc_9 = '  fo   12   bar'
+        config_9 = {
+            'f1': {'length': 4, 'type': str, 'default_value' : ''},
+            'f2': {'length': 5, 'type': int, 'default_value' : -1},
+            'f3': {'length': 6, 'type': str, 'default_value' : ''}
+        }
+
+        EA_9 ='  fo,   12,   bar\n'
+        self.assertEqual(FWF2CSV.fwf_2_csv_line(FWF2CSV(tc_9,config_9),tc_9,config_9),EA_9) 
+
+    def test_E2E(self):
+        # test case with all kind of problems
+        # the file should show correctly in excel while with problem in other utf-8 based apps
+        tc_10 = {
+            'f1': ['ab','‘’','AAABBB_B'],# big cell
+            'f2': [12.3,123,True], # incompatible cell type
+            'f3': ['ef','EF','EFFF_FFF'],# big cell
+            'f4': ['gh','','GHGHGHGH'], # empty cell
+            'f5': ['Jânis','El Niño','résumé'],
+            'f6': ['kl','KL'], # missing cell
+            'f7': [10,12,20], # int to float
+            'f8': [12.3,1.23,2.8], # foat to int
+            'f9': ['qr','QR','QRqrQRqr'],
+            'f10': ['st','ST','STSTstST']
+        }
+        config_10 = {
+            'f1': {'length': 5, 'type': str, 'default_value': ''},
+            'f2': {'length': 12, 'type': str, 'default_value': ''},
+            'f3': {'length': 3, 'type': str, 'default_value': ''},
+            'f4': {'length': 2, 'type': str, 'default_value': ''},
+            'f5': {'length': 13, 'type': str, 'default_value': ''},
+            'f6': {'length': 7, 'type': str, 'default_value': ''},
+            'f7': {'length': 10, 'type': float, 'default_value': ''},
+            'f8': {'length': 13, 'type': int, 'default_value': ''},
+            'f9': {'length': 20, 'type': str, 'default_value': ''},
+            'f10': {'length': 13, 'type': str, 'default_value': ''}
+        }
+
+        file_name = 'E2E_test_new_file.txt'
+        tc_10_insctance = FWF2CSV(tc_10,config_10)
+
+        FWF2CSV.write_file(tc_10_insctance,
+        tc_10, config_10, file_name=file_name)
+        self.assertTrue(os.path.exists(file_name))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
